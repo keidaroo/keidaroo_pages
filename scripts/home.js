7250b3a6e641d6ae2f1c4fb68ad430ec42ae4171
keidaroo <you@example.com>
2018-02-25 22:59:50 +0900

    set transform

--- scripts/home.js
+++ scripts/home.js
@@ -6,7 +6,7 @@ $(function() {
 $(document).ready(function() {
   setTimeout(function() {
     logoName.animate({
-      right: "60vw"
+      right: "54vw"
     }, 3000, "easeOutQuart");
 
   }, 1500);
@@ -15,9 +15,11 @@ $(document).ready(function() {
     var logoNameStretch = parseInt(logoName.css('font-stretch'));
     var logoNameWidth = parseInt(logoName.css('width'));
     var logoNameAbove = (logoNameWidth - logoNameStretch) / 2;
-    console.log(logoNameWidth);
-    console.log(logoNameWidth - logoNameAbove);
     logoSub.css('left', logoNameWidth);
     logoSub.fadeIn(700);
   }, 3000);
 });
+$(window).resize(function() {
+  logoName.css('right', '54vw');
+  logoSub.css('left', logoName.css('width'));
+})
