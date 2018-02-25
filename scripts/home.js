6fbdf3a33df3a9103c50b2cd885981b6ad0cc390
keidaroo <keiforprogramming@gmail.com>
2018-02-25 20:40:34 +0900

    made animation center

--- scripts/home.js
+++ scripts/home.js
@@ -6,12 +6,18 @@ $(function() {
 $(document).ready(function() {
   setTimeout(function() {
     logoName.animate({
-      right: 300
+      right: "60vw"
     }, 3000, "easeOutQuart");
 
   }, 700);
-
   setTimeout(function() {
+    var logoNameLeft = parseInt(logoName.css('left'));
+    var logoNameStretch = parseInt(logoName.css('font-stretch'));
+    var logoNameWidth = parseInt(logoName.css('width'));
+    var logoNameAbove = (logoNameWidth - logoNameStretch) / 2;
+    console.log(logoNameWidth);
+    console.log(logoNameWidth - logoNameAbove);
+    logoSub.css('left', logoNameWidth);
     logoSub.fadeIn(700);
-  }, 2000);
+  }, 3000);
 });
