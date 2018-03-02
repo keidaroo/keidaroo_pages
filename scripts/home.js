7aabf702c6840275e23f6dfe9b9e93b98959e17e
keidaroo <you@example.com>
2018-03-03 01:59:18 +0900

    changed style

--- scripts/home.js
+++ scripts/home.js
@@ -1,63 +1,29 @@
 var logoName = $(".logoName");
-var logoSub = $(".logoSub");
-var profileTextTitle = $(".profileTextTitle");
-var profileText = $(".profileText");
+var wallpaper = $("#home-wallpaper");
+var windowHeight = 0;
+var windowWidth = 0;
+
 $(function() {
-  var windowHeight = parseInt($(window).height());
-  var windowWidth = parseInt($(window).width());
-  logoName.css('font-size', Math.min(0.09 * windowWidth, 0.12 * windowHeight));
-  logoSub.css('font-size', Math.min(0.09 * windowWidth, 0.12 * windowHeight));
-  profileTextTitle.css('font-size', Math.min(0.06 * windowWidth, 0.1 * windowHeight));
-  profileText.css('font-size', Math.min(0.04 * windowWidth, 0.07 * windowHeight));
-  logoSub.hide();
-  profileText.hide();
-  profileTextTitle.hide();
+  windowHeight = parseInt($(window).height());
+  windowWidth = parseInt($(window).width());
+
+  logoName.css('font-size', Math.min(0.06 * windowWidth, 0.08 * windowHeight));
+
+  wallpaper.css('width', windowWidth);
+  wallpaper.css('height', windowHeight);
+
+  logoName.hide();
 })
 
 $(document).ready(function() {
-  setTimeout(function() {
-    var logoNameStretch = parseInt(logoName.css('font-stretch'));
-    var logoSubStretch = parseInt($(window).width()) / 4.6;
-    var above = parseInt($(window).width()) - logoSubStretch - logoNameStretch;
-    logoName.animate({
-      right: $(window).width() / 2
-    }, 3000, "easeOutQuart");
-
-  }, 1500);
-  setTimeout(function() {
-    var logoNameLeft = parseInt(logoName.css('left'));
-    var logoNameStretch = parseInt(logoName.css('font-stretch'));
-    var logoNameWidth = parseInt(logoName.css('width'));
-    var logoNameAbove = (logoNameWidth - logoNameStretch) / 2;
-    logoSub.css('left', logoNameWidth);
-
-    logoSub.fadeIn(700);
-  }, 3000);
-  setTimeout(function() {
-    var animateTop = parseInt(logoName.css('top')) * 0.3;
-    logoName.animate({
-      top: animateTop
-    }, {
-      duration: 700,
-      easing: 'easeOutQuart',
-      queue: false
-    });
-    logoSub.animate({
-      top: animateTop
-    }, {
-      duration: 700,
-      easing: 'easeOutQuart',
-      queue: false
-    })
-  }, 4300);
-  setTimeout(function() {
-    profileTextTitle.fadeIn(700);
-  }, 4900);
-  setTimeout(function() {
-    profileText.fadeIn(700);
-  }, 5700);
+  logoName.fadeIn(1200);
 });
 $(window).resize(function() {
-  logoName.css('right', $(window).width() / 2);
-  logoSub.css('left', logoName.css('width'));
+  windowHeight = parseInt($(window).height());
+  windowWidth = parseInt($(window).width());
+
+  logoName.css('font-size', Math.min(0.06 * windowWidth, 0.08 * windowHeight));
+  wallpaper.css('width', windowWidth);
+  wallpaper.css('height', windowHeight);
+
 })
