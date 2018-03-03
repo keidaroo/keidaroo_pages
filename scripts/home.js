6e1845b5daa3cf64e506531ef0955823243196a9
keidaroo <keiforprogramming@gmail.com>
2018-03-03 14:41:17 +0900

    font testing

--- scripts/home.js
+++ scripts/home.js
@@ -1,9 +1,11 @@
 var logoName = $(".logoName");
 var wallpaper = $("#home-wallpaper");
+var rocketImage = $(".rocketImage");
 var windowHeight = 0;
 var windowWidth = 0;
 
-$(function() {
+function sizeReset() {
+
   windowHeight = parseInt($(window).height());
   windowWidth = parseInt($(window).width());
 
@@ -11,6 +13,17 @@ $(function() {
 
   wallpaper.css('width', windowWidth);
   wallpaper.css('height', windowHeight);
+
+  var rocketMulti = 0.3;
+  var rocketSize = Math.min(rocketMulti * windowWidth, rocketMulti * windowHeight);
+  rocketImage.css('background-size', rocketSize);
+  rocketImage.css('height', rocketSize);
+  rocketImage.css('width', rocketSize);
+
+}
+
+$(function() {
+  sizeReset();
 
   logoName.hide();
 })
@@ -19,11 +32,5 @@ $(document).ready(function() {
   logoName.fadeIn(1200);
 });
 $(window).resize(function() {
-  windowHeight = parseInt($(window).height());
-  windowWidth = parseInt($(window).width());
-
-  logoName.css('font-size', Math.min(0.06 * windowWidth, 0.08 * windowHeight));
-  wallpaper.css('width', windowWidth);
-  wallpaper.css('height', windowHeight);
-
+  sizeReset();
 })
