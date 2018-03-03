38689ab62761ad00678fed7d3c1eea0e9d746d99
keidaroo <keiforprogramming@gmail.com>
2018-03-03 15:31:50 +0900

    color test

--- scripts/home.js
+++ scripts/home.js
@@ -9,12 +9,12 @@ function sizeReset() {
   windowHeight = parseInt($(window).height());
   windowWidth = parseInt($(window).width());
 
-  logoName.css('font-size', Math.min(0.06 * windowWidth, 0.08 * windowHeight));
+  logoName.css('font-size', Math.min(0.08 * windowWidth, 0.08 * windowHeight));
 
   wallpaper.css('width', windowWidth);
   wallpaper.css('height', windowHeight);
 
-  var rocketMulti = 0.3;
+  var rocketMulti = 0.4;
   var rocketSize = Math.min(rocketMulti * windowWidth, rocketMulti * windowHeight);
   rocketImage.css('background-size', rocketSize);
   rocketImage.css('height', rocketSize);
@@ -34,3 +34,19 @@ $(document).ready(function() {
 $(window).resize(function() {
   sizeReset();
 })
+var granimInstance = new Granim({
+  element: '#rocketCanvas',
+  name: 'basic-gradient',
+  direction: 'left-right', // 'diagonal', 'top-bottom', 'radial'
+  opacity: [1, 1],
+  isPausedWhenNotInView: true,
+  states: {
+    "default-state": {
+      gradients: [
+        ['#16d4b7', '#16d4b7'],
+        ['#b00294', '#b00294'],
+        ['#ffc822', '#ffc822']
+      ]
+    }
+  }
+});
