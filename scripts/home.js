885df9a0a2a65d4a191344de328a52ed25ad6456
keidaroo <you@example.com>
2018-03-07 04:02:00 +0900

    add random but still bug on position

--- scripts/home.js
+++ scripts/home.js
@@ -1,8 +1,11 @@
 var logoName = $(".logoName");
 var wallpaper = $("#home-wallpaper");
 var rocketImage = $(".rocketImage");
+var sceneObj = $(".scene");
 var windowHeight = 0;
 var windowWidth = 0;
+var scene = $(".scene").get(0);
+var parallaxInstance = new Parallax(scene);
 
 function sizeReset() {
 
@@ -14,6 +17,11 @@ function sizeReset() {
   wallpaper.css('width', windowWidth);
   wallpaper.css('height', windowHeight);
 
+
+  sceneObj.css('width', windowWidth);
+  sceneObj.css('height', windowHeight);
+
+
   var rocketMulti = 0.4;
   var rocketSize = Math.min(rocketMulti * windowWidth, rocketMulti * windowHeight);
   rocketImage.css('background-size', rocketSize);
@@ -24,6 +32,14 @@ function sizeReset() {
 
 $(function() {
   sizeReset();
+  var min = 0;
+  var max = 3;
+
+  var ran = Math.floor(Math.random() * (max + 1 - min)) + min;
+  var rocketName = ['rocketBlue', 'rocketRed', 'rocketGreen', 'rocketYellow'];
+  var backgroundUrl = 'url("pictures/png/' + rocketName[ran] + '.png ")';
+  console.log(backgroundUrl);
+  rocketImage.css('background-image', backgroundUrl);
 
   logoName.hide();
 })
